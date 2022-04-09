21c351a2fdde99f616a7343cc8d1922bf512c36f
angel practicum student <malahkc@yahoo.com>
2022-04-10 01:02:56 +0300

    with java

--- README.md
+++ README.md
@@ -2,15 +2,22 @@
 
 ### Overview
 
-* Figma
-* Images
+- Figma
+- Project Features
+- GitHub Pages
 
 **Figma**
 
-* [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
+- [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
 
-**Images**
+**Project Features**
 
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
+- HTML semantics
+- Grid Layout
+- Flex
+- positioning
+- Web Responsive
+- Flat BEM
+- JAVA Script
 
-Have fun with JavaScript!
+- Link to Project https://bdangel1.github.io/web_project_4/
---
+++ index.js
@@ -0,0 +1,27 @@
+const openEditModalButton = document.querySelector(".profile__button-edit");
+const modal = document.querySelector(".modal");
+const closeEditModalButton = document.querySelector(".modal__button");
+const inputName = document.querySelector(".form__input_type_name");
+const inputJob = document.querySelector(".form__input_type_job");
+const profileName = document.querySelector(".profile__info-name");
+const profileJob = document.querySelector(".profile__info-job");
+const form = document.querySelector(".form");
+function openModal() {
+  modal.classList.add("modal_open");
+}
+function closeModal() {
+  modal.classList.remove("modal_open");
+}
+openEditModalButton.addEventListener("click", function () {
+  openModal();
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+});
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.textContent = inputNameValue;
+  profileJob.textContent = inputJobValue;
+  closeModal();
+});
+closeEditModalButton.addEventListener("click", closeModal);
+closeModal();
